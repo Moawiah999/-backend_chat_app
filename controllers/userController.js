09ba1efb52eb6c88fc861fc5beaf2a2e02549243
moawiah <muawiyah454@gmail.com>
2026-04-01 17:03:22 +0300

    update message

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,9 @@ const userService = require("../services/userService");
 const register = async (req, res) => {
   try {
     const newUser = await userService.registerUser(req.body);
-    res.status(201).json({ success: true, user: newUser });
+    res
+      .status(201)
+      .json({ success: true, message: "Account creation successful" });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
